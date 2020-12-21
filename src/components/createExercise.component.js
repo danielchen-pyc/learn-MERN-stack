e56f44ebd7926c2ab415605422913cf46059e46d
danielchen-pyc <daniel7711potter@gmail.com>
2020-12-21 11:35:41 +0800

    Finished Frontend

--- src/components/createExercise.component.js
+++ src/components/createExercise.component.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css"
 
 export default class createExercise extends Component {
     constructor(props) {
@@ -18,6 +21,20 @@ export default class createExercise extends Component {
             users: [],
         }
     }
+    
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/users/')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
+    }
+
 
     onChangeUsername(e) {
         this.setState({
@@ -56,15 +73,62 @@ export default class createExercise extends Component {
             date: this.state.date,
         }
 
-        console.log(exercise);
-
-        window.location = '/';
+        axios.post('http://localhost:3001/exercises/add', exercise)
+            .then(res => console.log(res.data));
+        
+        window.location = 'http://localhost:3001/exercises/add';
     }
 
     render() {
         return (
             <div>
-                <p>You are on Create Exercise Component!</p>
+                <h3>Create New Exercise Log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <select ref="userInput" 
+                            required 
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}>
+                            {
+                                this.state.users.map(function(user) {
+                                    return <option key={user} value={user}>
+                                        {user}
+                                    </option>;
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.onChangeDescription}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Duration: </label>
+                        <input type="number"
+                            required
+                            className="form-control"
+                            value={this.state.duration}
+                            onChange={this.onChangeDuration}/>
+                    </div>
+                    <div className="form-group">
+                        <label>Date: </label>
+                        <div>
+                            <DatePicker
+                                selected={this.state.date}
+                                onChange={this.onChangeDate}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Exercise Log" className="btn btn-primary"></input>
+                    </div>
+                </form>
             </div>
         );
     };
--- src/components/exercisesList.component.js
+++ src/components/exercisesList.component.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+
+const Exercise = props => (
+    <tr>
+        <td>(props.exercise.username)</td>
+        <td>(props.exercise.description)</td>
+        <td>(props.exercise.duration)</td>
+        <td>(props.exercise.date.substring(0, 10))</td>
+        <td>
+            <Link to={"/edit/" + props.exercise._id}>Edit</Link> 
+                | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>Delete</a>
+        </td>
+    </tr>
+)
+
 
 export default class exercisesList extends Component {
+    constructor(props) {
+        super(props);
+        this.deleteExercise = this.deleteExercise.bind(this);
+        this.state = { exercises: [] };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/exercises/')
+            .then(response => {
+                this.setState({ exercises: response.data })
+            })
+            .catch(err => console.log(err));
+    }
+
+
+    deleteExercise(id) {
+        axios.delete('http://localhost:3001/exercises/', id)
+            .then(res => console.log(res));
+        this.setState({
+            exercises: this.state.exercises.filter(el => el._id !== id)
+        });
+    }
+
+
+    exercisesList() {
+        return this.state.exercises.map(currentExercises => {
+            return <Exercise exercises={currentExercises} deleteExercise={this.deleteExercise} key={currentExercises._id}/>;
+        })
+    }
+
+
     render() {
         return (
             <div>
-                <p>You are on Exercise List Component!</p>
+                <h3>Logged Exerciese</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Desciption</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.exercisesList() }
+                    </tbody>
+                </table>
             </div>
         );
     };
